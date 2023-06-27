32cd8de1aef7a633e44833427ac082029f25e4a4
padfoot-16 <amigo_awini@hotmail.com>
2023-06-27 14:59:35 +0100

    initial commit

---
+++ android/app/src/main/kotlin/com/example/managmentui/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.managmentui
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/screens/home.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/src/widgets/framework.dart';
+
+class Homepage extends StatelessWidget {
+  
+
+  @override
+  Widget build(BuildContext context) {
+    return  Scaffold(
+      body: Center(child: Text("holaaazzaa"),),
+    );
+  }
+}
